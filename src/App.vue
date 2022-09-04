3852cab79e1ad65f76938768fe4c28fb5bb34182
Toriola Faidat <faaedah@gmail.com>
2022-09-04 16:40:28 +0100

    added song styles

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,26 @@ export default {
 
 <style>
 * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
 }
 
 .flex {
 	display: flex;
 	align-items: center;
+}
+
+p {
+    color: white;
+}
+
+.circle {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 25px;
+    background: linear-gradient(180deg, #7C3C87 0%, rgba(196, 196, 196, 0) 100%);
+    justify-content: center;
+    z-index: 99;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-    <h1>Helooo</h1>
+    <div class="home">
+        <div class="song flex">
+            <div>
+                <p class="song-name">Burdha</p>
+                <p class="artist-name">Sami Yusuf</p>
+            </div>
+            <div class="circle flex">
+                <img src="./../assets/icons/play-btn.svg" class="play-btn"/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +18,24 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    .home {
+        height: 100vh;
+        width: 100%;
+        padding: 2rem 1rem; 
+        background: linear-gradient(135deg, hsla(226, 49%, 19%, 1) 0%, hsla(288, 56%, 26%, 1) 100%);
+        /*background: linear-gradient(135deg, hsla(0, 0%, 0%, 1) 7%, hsla(288, 56%, 26%, 1) 100%);*/
+    }
 
+    .song {
+        justify-content: space-between;
+        width: 90vw;
+        height: 70px;
+        background: radial-gradient(102.06% 345.4% at 50% 34.02%, 
+        rgba(87, 29, 102, 0.62) 0%,
+        #223053 100%);
+        margin: 1rem auto;
+        border-radius: 15px;
+        padding: 15px;
+    }
 </style>
