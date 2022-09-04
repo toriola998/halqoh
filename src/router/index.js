9edc24217bd7030a12fad10ce12b409333669fb0
Toriola Faidat <faaedah@gmail.com>
2022-09-04 12:56:10 +0100

    installed and configured route

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+import Home from './../views/Home.vue'
+
+
+const router = createRouter({
+    history:  createWebHistory(),
+    scrollBehavior() {
+      // always scroll to top
+      return { 
+          top: 0,
+          behavior: 'smooth', 
+     }
+   },
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        }, 
+    ]
+})
+export default router;
